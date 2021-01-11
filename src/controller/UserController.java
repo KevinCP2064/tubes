b947b1ed42cc2c77fd44385673541d3d4f534c4d
alfin7234 <60917377+alfin7234@users.noreply.github.com>
2021-01-12 00:26:39 +0700

    Update UserController.java by Alfin Liunardi

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import animatefx.animation.FadeIn;
+import com.mysql.cj.x.protobuf.MysqlxCrud;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.MouseEvent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,6 +14,11 @@ import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserController implements Initializable {
     @FXML
@@ -46,8 +56,78 @@ public class UserController implements Initializable {
     private void btnUpdateUserAction(ActionEvent actionEvent) {
     }
 
+    //----------------------------------------------dari sini part fxml untuk login-------------------------------------
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button btnSignUp;
+
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void mouseLogin(MouseEvent event) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/users?" + "user=root&password=password");
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE username =? AND password =?");
+            ps.setString(1, txtUser.getText());
+            ps.setString(2, txtPassword.getText());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(null, "You are logged in");
+            } else {
+                JOptionPane.showMessageDialog(null, "Wrong password or username");
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        if (event.getSource() instanceof Button) {
+            if (event.getSource() == btnLogin) {
+                new FadeIn(btnLogin).play();
+            }
+        }
+    }
+    //-----------------------------------------------sampai sini part fxml untuk login---------------------------------
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @Override //--------- ini @Override untuk login
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    //------------------------------------------------ini program untuk login
+
+    public void mouseSignUp(MouseEvent em) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/users?" + "user=root&password=password");
+            PreparedStatement pss = con.prepareStatement("INSERT INTO users SET username =? , password =?");
+            pss.setString(1, txtUser.getText());
+            pss.setString(2, txtPassword.getText());
+            pss.executeUpdate();
+        } catch (Exception ee) {
+            JOptionPane.showMessageDialog(null, "Wrong password or username");
+        } finally {
+            JOptionPane.showMessageDialog(null, "Success . You have created an account.");
+        }
+        if (em.getSource() instanceof Button) {
+            if (em.getSource() == btnSignUp) {
+                new FadeIn(btnSignUp).play();
+            }
+        }
+    }
+
+    //---------------------------------------------------------sampai sini program untuk login
+
 }
